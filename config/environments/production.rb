b841f56eaaceac84be0b9f65e10f2a1c63c8020b
Sharmila <das.sharmila@gmail.com>
2018-12-07 01:31:50 +0000

    User authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { host: 'nomster-sharmila-das.herokuapp.com'}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -27,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.serve.static_assets = true
+  config.serve_static_assets = true
   config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
